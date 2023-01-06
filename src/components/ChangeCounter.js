d15f02fb430d576283e3249b6ac524c128a110ca
danimlds <daniel.mlds@gmail.com>
2023-01-06 20:12:42 -0300

    Example context-API

---
+++ src/components/ChangeCounter.js
@@ -0,0 +1,19 @@
+
+
+import { useCounterContext } from "../hooks/useCounterContext";
+
+const ChangeCounter = () => {
+  // const { counter, setCounter } = useContext(CounterContext);
+  // 4 - refatorando para hook
+  const { counter, setCounter } = useCounterContext();
+
+  return (
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>
+        Add value to counter
+      </button>
+    </div>
+  );
+};
+
+export default ChangeCounter;
---
+++ src/pages/Home.js
@@ -0,0 +1,34 @@
+import "./Home.css";
+
+import { useContext } from "react";
+
+import { CounterContext } from "../context/CounterContext";
+import ChangeCounter from "../components/ChangeCounter";
+
+// 4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
+
+const Home = () => {
+    const {counter} = useCounterContext();
+
+    const {color, dispatch} = useTitleColorContext();
+
+    const setTitleColor = (color) => {
+        dispatch({type: color});
+    };
+
+  return (
+    <div>
+        <h1 style={{color: color}}>Home</h1>
+        <p>Valor contador: {counter}</p>
+        <ChangeCounter />
+        <div>
+            <button onClick={() => setTitleColor("RED")}>Vermelho</button>
+            <button onClick={() => setTitleColor("BLUE")}>Azul</button>
+        </div>
+    </div>
+  );
+};
+
+export default Home
